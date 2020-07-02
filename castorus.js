f982192959a1d95b70510b4030b0dd7b340f2559
andreas <github@goodk.at>
2020-07-03 00:27:48 +0200

    update

--- castorus.js
+++ castorus.js
@@ -6,11 +6,11 @@ var finderConfig = path.join(__dirname, './config/finderConfig.json');
 var jsonfile = require('jsonfile');
 
 jsonfile.readFile(finderConfig, function (err, finder) {
-	if (err) return handleError(err);
+	if (err) return console.log(err);
 
-for (var i=0;i<finder.country.length;i++){
-        var list=finder.country[i];
-        setTimeout(getDetails,i*500,list,finder,function(err,countryDetail){
+for (var i=0;i<finder.city.length;i++){
+        var list=finder.city[i];
+        setTimeout(getDetails,i*500,list,finder,function(err,cityDetail){
            
         });
         
@@ -24,10 +24,11 @@ function getDetails (list,finder,callback)
             'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:76.0) Gecko/20100101 Firefox/76.0',
             'Accept: text/html'
         ])
-        .get('https://www.castorus.com/s/'+list.link+"--"+finder.surfaceMin+"--"+finder.prixMin+"-"+finder.prixMax+"-----------------------")
+        .get('https://www.castorus.com/s/'+list.linkCastorus+"--"+finder.surfaceMin+"--"+finder.prixMin+"-"+finder.prixMax+"-----------------------")
         .then(({statusCode, body, headers}) => {
 
             htmlToJson.parse(body, function () {
+                // console.log(body)
                 var i = 0;
                 var j=0;
                 return this.map('.price', function (item) {
@@ -60,9 +61,11 @@ function getDetails (list,finder,callback)
             htmlToJson.parse(body, function () {
                 var i=0;
                 return this.map('.surf', function (item) {
+                    buyList[i]=Object.assign(buyList[i],JSON.parse(JSON.stringify({"codePostal":list.codePostal})));
                     buyList[i]=Object.assign(buyList[i],JSON.parse(JSON.stringify({"surface":item.text()})));
+                    // console.log(buyList[i])
                     i++;
-                    console.log(i)
+                    
                 });
             }).done(function (items) {
                 // Items should be: ['1','2']
@@ -70,6 +73,41 @@ function getDetails (list,finder,callback)
             }, function (err) {
                 // Handle error
             });
+            
+
+            htmlToJson.parse(body, function () {
+                var i=0;
+                return this.map('.since', function (item) {
+                    buyList[i]=Object.assign(buyList[i],JSON.parse(JSON.stringify({"depuis":item.text()})));
+                    // console.log(buyList[i])
+                    i++;
+                    
+                });
+            }).done(function (items) {
+                // Items should be: ['1','2']
+                // console.log(items);
+            }, function (err) {
+                // Handle error
+            });
+
+            htmlToJson.parse(body, function () {
+                var i=0;
+                return this.map('.title', function (item) {
+                    console.log(JSON.parse(item.children));
+                    buyList[i]=Object.assign(buyList[i],JSON.parse(JSON.stringify({"lien":item.attr('a')})));
+                    console.log(buyList[i])
+                    i++;
+                    
+                });
+            }).done(function (items) {
+                // Items should be: ['1','2']
+                // console.log(items);
+            }, function (err) {
+                // Handle error
+            });
+
+
+
             //console.log(statusCode, body, headers)
         })
         .catch((e) => {
